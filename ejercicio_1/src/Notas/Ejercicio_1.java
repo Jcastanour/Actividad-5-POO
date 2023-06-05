af1c0a0fcd466a1c158e644b62e0939831ed2b2d
jcastanour <jcastanour@unal.edu.co>
2023-06-04 21:09:30 -0500

    ejercicio1

---
+++ ejercicio_1/src/Notas/Ejercicio_1.java
@@ -0,0 +1,16 @@
+
+package Notas;
+
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana
+        principal */
+        miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana
+        principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana
+        como visible */
+       
+    }
+
+}
